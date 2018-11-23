ef21eb8bf153d2f1035365d93422776e86047322
Richard <rhshadowgreen@gmail.com>
2018-11-23 15:06:49 -0500

    Removed season relative numbers

--- AdvTimeScript.py
+++ AdvTimeScript.py
@@ -7,8 +7,8 @@ for line in lines:
     x = line.split("\t")
 
     if even:
-        overall, season, title = x[0], x[1], x[2]
-        row = "- [ ] " + overall + ", " + season + ", " + title
+        overall, title = x[0], x[2]
+        row = "- [ ] " + overall + ", " + title
         print(row)
 
     even = not even
